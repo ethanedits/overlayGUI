0d587aa6fa0f3f179a128d5cd6603470463523ec
ethanedits <58463523+ethanedits@users.noreply.github.com>
2020-11-19 09:13:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ oGUI.init()
 checkbox = oGUI.Checkbox(oGUI.gray, oGUI.orange, 125, 150, 20, 20)
 rect = oGUI.Rect(oGUI.darkgray, 100, 100, 300, 500)
 box = oGUI.Box(oGUI.lightgray, 100, 100, 300, 500, 5)
+myText = oGUI.Text(oGUI.orange, 195, 110, 30, "oGUI Demo")
+myText2 = oGUI.Text(oGUI.orange, 155, 152, 25, "Checkbox")
 
 while True:
 
@@ -13,9 +15,17 @@ while True:
     rect.draw()
     box.draw()
     checkbox.draw()
+    myText.font('Roboto')
+    myText.draw()
+    myText2.font('Roboto')
+    myText2.draw()
 
     oGUI.endLoop()
 
     checkbox.is_hovered(oGUI.lightgray)
+
+    myText.dropShadow(oGUI.black, 2)
+    myText2.dropShadow(oGUI.black, 2)
+
     if checkbox.is_enabled():
         print('Checkbox is Enabled!')
