8b8dffbce99f7ad02fdba6f558d811aca69ed335
ethanedits <58463523+ethanedits@users.noreply.github.com>
2020-11-21 03:39:45 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ box = oGUI.Box(oGUI.lightgray, 100, 100, 300, 500, 5)
 myText = oGUI.Text(oGUI.orange, 195, 110, 30, "oGUI Demo")
 myText2 = oGUI.Text(oGUI.orange, 155, 152, 25, "Checkbox")
 
+btn = oGUI.Button(oGUI.orange, 125, 200, 100, 100)
+
 while True:
 
     oGUI.startLoop()
@@ -19,13 +21,18 @@ while True:
     myText.draw()
     myText2.font('Roboto')
     myText2.draw()
+    btn.draw()
 
     oGUI.endLoop()
 
     checkbox.is_hovered(oGUI.lightgray)
+    btn.is_hovered(oGUI.lightgray)
 
     myText.dropShadow(oGUI.black, 2)
     myText2.dropShadow(oGUI.black, 2)
+    
+    if btn.is_enabled():
+        print('Button was pressed')
 
     if checkbox.is_enabled():
         print('Checkbox is Enabled!')
