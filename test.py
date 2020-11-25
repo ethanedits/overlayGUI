36690bcafff9c5c86afeec159db56710194d2b74
ethanedits <58463523+ethanedits@users.noreply.github.com>
2020-11-24 19:17:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,34 +5,48 @@ oGUI.init()
 checkbox = oGUI.Checkbox(oGUI.gray, oGUI.orange, 125, 150, 20, 20)
 rect = oGUI.Rect(oGUI.darkgray, 100, 100, 300, 500)
 box = oGUI.Box(oGUI.lightgray, 100, 100, 300, 500, 5)
+button = oGUI.Button(oGUI.gray, oGUI.orange, 120, 200, 30, 30)
+button2 = oGUI.Button(oGUI.darkgray, oGUI.lightgray, 368, 103, 30, 35)
+
 myText = oGUI.Text(oGUI.orange, 195, 110, 30, "oGUI Demo")
 myText2 = oGUI.Text(oGUI.orange, 155, 152, 25, "Checkbox")
-
-btn = oGUI.Button(oGUI.orange, 125, 200, 100, 100)
+myText3 = oGUI.Text(oGUI.orange, 155, 208, 25, "Button")
+myText4 = oGUI.Text(oGUI.black, 375, 105, 30, "X")
 
 while True:
 
-    oGUI.startLoop()
+    oGUI.startLoop() #Start of Draw Loop
 
-    rect.draw()
+    rect.draw() #Drawing Rectangle, Box, Checkbox, and Button
     box.draw()
     checkbox.draw()
-    myText.font('Roboto')
-    myText.draw()
-    myText2.font('Roboto')
+    button.draw()
+    button2.draw()
+
+    myText.draw() #Drawing Text
     myText2.draw()
-    btn.draw()
+    myText3.draw()
+    myText4.draw()
 
-    oGUI.endLoop()
+    oGUI.endLoop() #End of Draw Loop
 
-    checkbox.is_hovered(oGUI.lightgray)
-    btn.is_hovered(oGUI.lightgray)
+    checkbox.is_hovered(oGUI.lightgray) #Changes color when checkbox and button(s) is hovered over
+    button.is_hovered(oGUI.lightgray)
+    button2.is_hovered(oGUI.gray)
 
-    myText.dropShadow(oGUI.black, 2)
+    myText.font('Roboto') #Setting Text Object's font
+    myText2.font('Roboto')
+    myText3.font('Roboto')
+
+    myText.dropShadow(oGUI.black, 2) #Setting Text Object's DropShadow
     myText2.dropShadow(oGUI.black, 2)
+    myText3.dropShadow(oGUI.black, 2)
     
-    if btn.is_enabled():
+    if button.is_enabled(): #Do something if the button is enabled/pressed
         print('Button was pressed')
 
-    if checkbox.is_enabled():
+    if button2.is_enabled(): #Exit Button
+        exit(0)
+
+    if checkbox.is_enabled(): #Do something if the checkbox is enabled
         print('Checkbox is Enabled!')
